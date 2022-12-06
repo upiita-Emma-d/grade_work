b0e6f46f7420d9c7d3cd74e5ac562018f2604d5b
Axe Diaz <axtherocknass@gmail.com>
2022-12-06 01:59:59 -0600

    delate fieles

--- qualityanalysis/analysis/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from analysis.models import Analisys90
-from organization.models import AnalisysCenter
-# Register your models here.
-class Analisys90Admin(admin.ModelAdmin):
-    pass
-admin.site.register(Analisys90)
--- qualityanalysis/organization/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from organization.models import AnalisysCenter
-# Register your models here.
-
-class AnalisysCenterAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(AnalisysCenter)
