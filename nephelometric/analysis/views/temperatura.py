bc4123bb9f404a6fc5e0e7cf674cccbce4ee8a8a
Axe Diaz <axtherocknass@gmail.com>
2022-12-21 20:40:14 -0600

    Add the index

--- nephelometric/analysis/views/temperatura.py
+++ nephelometric/analysis/views/temperatura.py
@@ -6,7 +6,7 @@ from analysis.helpers_analysis.get_messages import get_messages
 import serial
 import time
 
-
+from analysis.models import Muestra
 
 
 def temperatura_view(request):
@@ -14,17 +14,28 @@ def temperatura_view(request):
     port = serial_ports()
     print(port)
     try:
-        datos = main_arduino(port[1])
+        muestras = Muestra.objects.all()
+        min_vs = []
+        max_vs = []
+        v_prom = []
+        for muestra in muestras:
+            min_vs.append(muestra.min_voltage)
+            max_vs.append(muestra.max_voltage)
+            v_prom.append(muestra.voltaje_prom)
+            
+            
     except IndexError:
         datos = [0,0,0,0,0]
     welcome_message  = get_messages('bienvenida')
    
     project_name = get_messages('project_name')
      
-    puertos = serial_ports()
-    context = {'puertos': puertos,
+    #puertos = serial_ports()
+    context = {
                #'welcome_message':welcome_message,
-               'datos':datos,
+               'min_vs':min_vs,
+               'max_vs':max_vs,
+               'v_prom':v_prom,
                'project_name':project_name,
                }
     return render(request, 'sensor_template/temperatura.html', context)
