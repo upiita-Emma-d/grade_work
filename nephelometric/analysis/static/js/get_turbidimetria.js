6f8245802693b2ef768fb4ff907ac299431a7d06
Axe Diaz <axtherocknass@gmail.com>
2023-01-22 18:51:15 -0600

    Varios valores

--- nephelometric/analysis/static/js/get_turbidimetria.js
+++ nephelometric/analysis/static/js/get_turbidimetria.js
@@ -1,11 +1,16 @@
 
 const URL = 'https://sistema-calidad-del-agua.link/get_muestra_turbidimetria/';
-// const URL = 'http://127.0.0.1:8000/get_muestra_turbidimetria/';
-// http://127.0.0.1:8000/ph/
+
 fetch(URL)
   .then(res => res.json())
   .then(data => {
+    console.log(data);
+    const numbers = [];
+    const min_voltage = data.map(obj => obj.min_voltage);
+    const max_voltage = data.map(obj => obj.max_voltage);
     const voltaje_prom = data.map(obj => obj.voltaje_prom);
+    console.log(max_voltage);
+
     const fechas = data.map(obj => moment(obj.created_at).format("DD-MM-YYYY HH:mm:ss"));
     // Obtener una referencia al elemento canvas del DOM
     const $grafica = document.querySelector("#grafica");
@@ -15,39 +20,36 @@ fetch(URL)
         type: 'line',// Tipo de gráfica
         data: {
             labels: fechas,
-            datasets: [
+            datasets: [{
+                data: min_voltage,
+                borderColor: "blue",
+                fill: false,
+                label: 'Voltaje minimo',
+                },
+                {
+                data: max_voltage,
+                borderColor: "red",
+                fill: false,
+                label: 'Voltaje máximo',
+                },
                 {
                 data: voltaje_prom,
-                borderColor: "#F5E92C",
+                borderColor: "black",
                 fill: false,
-                label: 'Turbidimetria',
+                label: 'Voltaje promedio',
                 },
             ]
         },
-        // options: {
-        //     display: true,
-        //     scales: {
-        //         yAxes: [{
-        //             ticks: {
-        //                 beginAtZero: true
-        //             }
-        //         }],
-        //     },
-        // }
-
-
         options: {
-            responsive: true,
-            plugins: {
-              legend: {
-                position: 'top',
-              },
-              title: {
-                display: true,
-                text: 'Chart.js Line Chart'
-              }
-            }
-          }
+            display: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+            },
+        }
     });
     })
   .catch(error => {
